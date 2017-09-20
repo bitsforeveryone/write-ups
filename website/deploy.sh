f06023ae9c69b51980bf71584097ed29bb4d7bae
Roy Ragsdale <roy@ragsdale.xyz>
2017-09-19 20:46:43 -0400

    Add deployment script for GitHub pages

---
+++ website/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# reference: https://gohugo.io/hosting-and-deployment/hosting-on-github/#host-github-user-or-organization-pages
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project.
+hugo 
+
+# Go To Public folder and add changes to git
+cd public
+git add .
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+# Come Back up to the Project Root
+cd ..
