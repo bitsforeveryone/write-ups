5d96aaf1402b78fd63783db95f06ce2eff475b2a
Roy Ragsdale <roy@ragsdale.xyz>
2018-04-10 10:46:36 -0400

    Update deploy.sh to be more robust
    - clean/update prior to build

--- website/deploy.sh
+++ website/deploy.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 # reference: https://gohugo.io/hosting-and-deployment/hosting-on-github/#host-github-user-or-organization-pages
 
+set -e
+
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-# Build the project.
-hugo 
-
-# Go To Public folder and add changes to git
+# Go To Public folder and ensure we are on master
+echo "Update submodule"
 cd public
 # ensure we are at head
+git checkout -- ./*
+git clean -f -d
+git clean -f -x
 git checkout master
 git pull origin master
+
+# Build the project.
+cd ..
+echo "Build website with hugo"
+hugo 
+
+
 # add changes
+echo  "Add changes to submodule"
+cd  public
 git add .
 
 # Commit changes.
@@ -30,3 +42,5 @@ cd ..
 # Update submodule
 git add public
 git commit -m "deployed site `date`"
+
+echo "Make sure to push your current commit to repo"
